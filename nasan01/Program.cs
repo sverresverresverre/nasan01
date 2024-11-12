6577a8cc263cdce2a8af0014539943f63d7efa84
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2024-11-12 10:35:44 +0100

    shabbabab

---
+++ nasan01/Program.cs
@@ -0,0 +1,16 @@
+﻿if(6 >= 3)
+{
+    Console.WriteLine("Hello, World!");
+}
+
+Console.WriteLine("Write your name.");
+string username = Console.ReadLine();
+
+Console.WriteLine("Type in the password.");
+string password = Console.ReadLine();
+if(username == "kalleanka" && password == "12345")
+{
+    Console.WriteLine("Welcome!");
+}
+
+Console.ReadLine();
